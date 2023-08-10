446788d51b2b540dabdb9d1e1639e46ad1a50369
Sadhana Prajapati <122430191+sadhana-12345@users.noreply.github.com>
2023-08-10 17:12:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
+app=Flask(__name__)
+app.config['MYSQL_HOST']="localhost"
+app.config['MYSQL_USER']="root"
+app.config['MYSQL_PASSWORD']="Sadhu@2003"
+app.config['MYSQL_DB']="flask"
 
-app=Flask(__name__,static_folder='static')
-
-@app.route('/')
-def Hello_world():
+mysql=MySQL(app)
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == "POST":
+        name=request.form['name']
+        subject=request.form['subject']
+        email=request.form['email']
+        message=request.form['message']
+        cur_obj=mysql.connection.cursor()
+        cur_obj.execute("INSERT INTO cv (name,subject,email,message) VALUES(%s,%s,%s,%s)",(name,subject,email,message))
+        mysql.connection.commit()
+        cur_obj.close()
+        return "Data insert Successfully!!"
     return render_template('index.html')
-
-if __name__ == "__main__":
+    
+if __name__=="__main__":
     app.run(debug=True)
