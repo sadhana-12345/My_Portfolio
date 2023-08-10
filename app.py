678c236c101f8a66f32a6f13389c3e4b644811e0
Sadhana Prajapati <122430191+sadhana-12345@users.noreply.github.com>
2023-08-10 17:20:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 app=Flask(__name__)
 app.config['MYSQL_HOST']="localhost"
 app.config['MYSQL_USER']="root"
-app.config['MYSQL_PASSWORD']="Sadhu@2003"
+app.config['MYSQL_PASSWORD']=""
 app.config['MYSQL_DB']="flask"
 
 mysql=MySQL(app)
